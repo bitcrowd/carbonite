910652b27530b4f67985381eaef830c531154613
Malte Rohde <malte@bitcrowd.net>
2021-09-27 09:05:13 +0200

    add Query.changes/2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,8 @@
 
 * Store primary key(s) on changes table and index them
 * Add `Carbonite.Query` module
-* `current_transaction/2` allows to fetch the ongoing transaction (for sandbox tests)
+  - `current_transaction/2` allows to fetch the ongoing transaction (for sandbox tests)
+  - `changes/2` allows to fetch the changes of an invidual source record
 * Update Postgrex to 0.15.11 and drop local `Xid8` type
 
 ## [0.1.0] - 2021-09-01
--- test/query_test.exs
+++ test/query_test.exs
@@ -10,13 +10,17 @@ defmodule QuertTest do
     :ok = Sandbox.checkout(TestRepo)
   end
 
-  defp insert_transaction do
-    {:ok, _} =
+  defp insert_rabbits(_ \\ nil) do
+    {:ok, results} =
       Ecto.Multi.new()
       |> Carbonite.insert()
       |> Ecto.Multi.put(:params, %{name: "Jack", age: 99})
       |> Ecto.Multi.insert(:rabbit, &Rabbit.create_changeset(&1.params))
+      |> Ecto.Multi.put(:params2, %{name: "Lily", age: 172})
+      |> Ecto.Multi.insert(:rabbit2, &Rabbit.create_changeset(&1.params2))
       |> TestRepo.transaction()
+
+    Map.take(results, [:rabbit, :rabbit2])
   end
 
   defp count(schema) do
@@ -26,26 +30,26 @@ defmodule QuertTest do
   end
 
   describe "current_transaction/2" do
-    setup do
-      insert_transaction()
-      :ok
-    end
+    setup [:insert_rabbits]
 
     test "can fetch the current transaction when inside SQL sandbox" do
       assert [%Transaction{}] = TestRepo.all(Query.current_transaction())
     end
 
     test "can preload changes alongside the transaction" do
-      assert [%Transaction{changes: [%Change{}]}] =
+      assert [%Transaction{changes: [%Change{}, %Change{}]}] =
                TestRepo.all(Query.current_transaction(preload: :changes))
 
-      assert [%Transaction{changes: [%Change{}]}] =
+      assert [%Transaction{changes: [%Change{}, %Change{}]}] =
+               TestRepo.all(Query.current_transaction(preload: [:changes]))
+
+      assert [%Transaction{changes: [%Change{}, %Change{}]}] =
                TestRepo.all(Query.current_transaction(preload: true))
     end
 
     test "can be used to erase the current transaction" do
       assert count(Transaction) == 1
-      assert count(Change) == 1
+      assert count(Change) == 2
 
       TestRepo.delete_all(Query.current_transaction())
 
@@ -53,7 +57,26 @@ defmodule QuertTest do
       assert count(Change) == 0
 
       # No unique constraint error as transaction has been deleted.
-      insert_transaction()
+      insert_rabbits()
+    end
+  end
+
+  describe "changes/2" do
+    setup [:insert_rabbits]
+
+    test "queries changes for a given record given its struct", %{rabbit: rabbit} do
+      assert [%Change{new: %{"name" => "Jack"}}] = Query.changes(rabbit) |> TestRepo.all()
+    end
+
+    test "can preload the transaction", %{rabbit: rabbit} do
+      assert [%Change{transaction: %Transaction{}}] =
+               Query.changes(rabbit, preload: :transaction) |> TestRepo.all()
+
+      assert [%Change{transaction: %Transaction{}}] =
+               Query.changes(rabbit, preload: [:transaction]) |> TestRepo.all()
+
+      assert [%Change{transaction: %Transaction{}}] =
+               Query.changes(rabbit, preload: true) |> TestRepo.all()
     end
   end
 end
