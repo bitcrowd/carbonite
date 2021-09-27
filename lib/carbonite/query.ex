8bd7b949505b06dac884078ba960af941a9cadd1
Malte Rohde <malte@bitcrowd.net>
2021-09-27 09:05:13 +0200

    add Query.changes/2

--- lib/carbonite/query.ex
+++ lib/carbonite/query.ex
@@ -7,11 +7,12 @@ defmodule Carbonite.Query do
 
   import Ecto.Query, only: [from: 2, preload: 2, put_query_prefix: 2]
   import Carbonite, only: [default_prefix: 0]
-  alias Carbonite.Transaction
+  alias Carbonite.{Change, Transaction}
 
   @type prefix :: binary() | atom()
+  @type preload :: atom() | [atom()] | true
 
-  @type current_transaction_option :: {:prefix, prefix()} | {:preload, atom() | [atom()] | true}
+  @type current_transaction_option :: {:carbonite_prefix, prefix()} | {:preload, preload()}
 
   @doc """
   Returns an `Ecto.Query` that can be used to select or delete the "current" transaction.
@@ -49,23 +50,62 @@ defmodule Carbonite.Query do
 
   ## Options
 
-  * `preload` defines which associations to preload, defaults to none
-  * `prefix` defines the transaction log's schema, defaults to `"carbonite_default"`
+  * `carbonite_prefix` defines the transaction log's schema, defaults to `"carbonite_default"`
+  * `preload` can be used to preload the changes
   """
   @spec current_transaction() :: Ecto.Query.t()
   @spec current_transaction([current_transaction_option()]) :: Ecto.Query.t()
   def current_transaction(opts \\ []) do
-    prefix = Keyword.get(opts, :prefix, default_prefix())
+    carbonite_prefix = Keyword.get(opts, :carbonite_prefix, default_prefix())
 
     from(t in Transaction, where: t.id == fragment("pg_current_xact_id()"))
-    |> put_query_prefix(prefix)
-    |> maybe_preload(opts)
+    |> put_query_prefix(carbonite_prefix)
+    |> maybe_preload(opts, :changes)
   end
 
-  defp maybe_preload(queryable, opts) do
+  @default_table_prefix "public"
+
+  @type changes_option :: {:carbonite_prefix, prefix()} | {:preload, preload()}
+
+  @doc """
+  Returns an `Ecto.Query` that can be used to select changes for a single record.
+
+  Given an `Ecto.Schema` struct, this function builds a query that fetches all changes recorded
+  for it from the database, ordered ascending by their ID (i.e., roughly by insertion date
+  descending).
+
+  ## Options
+
+  * `carbonite_prefix` defines the transaction log's schema, defaults to `"carbonite_default"`
+  * `preload` can be used to preload the transaction
+  """
+  @spec changes(Ecto.Schema.t()) :: Ecto.Query.t()
+  @spec changes(Ecto.Schema.t(), [changes_option()]) :: Ecto.Query.t()
+  def changes(%schema{__meta__: %Ecto.Schema.Metadata{}} = record, opts \\ []) do
+    carbonite_prefix = Keyword.get(opts, :carbonite_prefix, default_prefix())
+
+    table_prefix = schema.__schema__(:prefix) || @default_table_prefix
+    table_name = schema.__schema__(:source)
+
+    table_pk =
+      for pk_col <- schema.__schema__(:primary_key) do
+        record |> Map.fetch!(pk_col) |> to_string()
+      end
+
+    from(c in Change,
+      where: c.table_prefix == ^table_prefix,
+      where: c.table_name == ^table_name,
+      where: c.table_pk == ^table_pk,
+      order_by: {:asc, :id}
+    )
+    |> put_query_prefix(carbonite_prefix)
+    |> maybe_preload(opts, :transaction)
+  end
+
+  defp maybe_preload(queryable, opts, default) do
     case Keyword.get(opts, :preload, []) do
       [] -> queryable
-      true -> preload(queryable, :changes)
+      true -> preload(queryable, ^default)
       preload -> preload(queryable, ^preload)
     end
   end
